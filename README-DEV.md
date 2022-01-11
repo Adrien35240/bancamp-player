54afc2ee8e99735a3148d8e2867dc4de1ef54255
Adrien35240 <adriendev35240@gmail.com>
2022-01-11 10:14:49 +0100

    fix(App.js):fix callback error on tab.query

---
+++ README-DEV.md
@@ -0,0 +1,2 @@
+## RESSOURCES
+(Continuous integration)[https://circleci.com/blog/continuously-deploy-a-chrome-extension/]
--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import './App.css';
 function App(){
   const [playingSongTitle,setPlayingSongTitle]=useState('');
 
-  useEffect(()=>{
-  })
-
   async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+    await chrome.tabs.query(queryOptions,(tab)=>{
+    console.log('tab query',tab)
+    return tab;
+    });
 }
 
  function injectTheScript() {
     const tab = getCurrentTab()
+   console.log('tab inject ',tab)
     chrome.tabs.executeScript(tab.id,{file:'./inject.js'}, function (data){
       console.log('retour script',data)
       setPlayingSongTitle(data[0])
