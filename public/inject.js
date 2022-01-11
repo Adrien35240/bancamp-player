2c9e5c6d01d9a1b0f77cfd858994b2b6da41c9f9
Adrien35240 <adriendev35240@gmail.com>
2022-01-11 01:01:05 +0100

    return the song title & display on extension

--- public/inject.js
+++ public/inject.js
@@ -1,7 +1,15 @@
-    console.log('play')
-    console.log('click')
-    const lists = document.querySelectorAll('.collection-item-container')
-console.log(lists)
+/* eslint-disable no-undef */
+(function(){
+let lists = null
+   if(!lists){
+  lists = document.querySelectorAll('.collection-item-container')
+       console.log(lists)
+   }
     lists[0].classList.add('playing')
     const btnPlay = lists[0].querySelector('.item_link_play_bkgd')
     btnPlay.click()
+  const songTitle = lists[0].querySelector('.collection-item-title').innerText
+  console.log('song title :',songTitle)
+return songTitle
+})()
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 /* eslint-disable no-undef */
+import {useEffect,useState} from 'react'
 import './App.css';
+
 function App(){
- async function getCurrentTab() {
+  const [playingSongTitle,setPlayingSongTitle]=useState('');
+
+  useEffect(()=>{
+  })
+
+  async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
 
-
-  function injectTheScript() {
-    console.log('inject script')
+ function injectTheScript() {
     const tab = getCurrentTab()
-    console.log('tab',tab)
-    chrome.tabs.executeScript(tab.id,{file:'./inject.js'})
+    chrome.tabs.executeScript(tab.id,{file:'./inject.js'}, function (data){
+      console.log('retour script',data)
+      setPlayingSongTitle(data[0])
+    })
 }
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="playBtn" onClick={injectTheScript}>Play</div>
+        <div className="songPlaying">{playingSongTitle}</div>
       </header>
     </div>
   );
